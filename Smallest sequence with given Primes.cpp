42d5f659a57e6837118d21cf52d7131e872ac804
vic006 <vedavikas06@gmail.com>
2018-10-02 04:32:16 +0530

    Create Smallest sequence with given Primes.cpp

---
+++ Smallest sequence with given Primes.cpp
@@ -0,0 +1,34 @@
+//BFS kind of problem
+vector<int> Solution::solve(int A, int B, int C, int D) {
+    
+    priority_queue<int,vector<int> ,greater<int> > p;
+    
+    vector<int> v;
+    v.push_back(A);v.push_back(B);v.push_back(C);
+    for(int i =0;i<v.size();i++)
+    p.push(v[i]);
+    
+    int cnt=0;
+    map<int,int> m;
+    vector<int> res;
+    while(cnt < D){
+        int tp = p.top();
+        p.pop();
+        
+        if(m.find(tp)!=m.end()){
+        res.push_back(tp);
+        
+        for(int i =0;i<v.size();i++){
+            p.push(v[i]*tp);
+        }
+        
+        cnt++;
+        m[tp] = 1;
+        }
+        
+        
+        
+    }
+     return res; 
+    
+}
